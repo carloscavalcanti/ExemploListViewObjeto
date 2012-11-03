ef0f1d321c2139cdefc8eb46ca476e913c89a0f8
Carlos Cavalcanti <carlos.cavalcanti@abacomm.com.br>
2012-11-03 19:06:30 -0300

    first commit

---
+++ src/com/example/exemplolistviewobjeto/ClienteAdapter.java
@@ -0,0 +1,70 @@
+package com.example.exemplolistviewobjeto;
+
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class ClienteAdapter extends BaseAdapter {
+	
+	private List<Cliente> clientes;
+	private LayoutInflater mInflater;
+	private ViewHolder holder;
+
+
+	static class ViewHolder{
+		private TextView txtNome;
+		private TextView txtDataNascimento;
+	}
+
+
+	public ClienteAdapter(Context context, List<Cliente> cliente) {
+		mInflater = LayoutInflater.from(context);
+		this.clientes = cliente;
+	}
+
+	@Override
+	public int getCount() {
+		return clientes.size();
+	}
+
+	@Override
+	public Object getItem(int index) {
+		return clientes.get(index);
+	}
+
+	@Override
+	public long getItemId(int index) {
+		return index;
+	}
+
+	@Override
+	public View getView(int posicao, View convertView, ViewGroup arg2) {
+
+		if (convertView == null) {
+			convertView = mInflater.inflate(R.layout.cliente_list, null);
+			holder = new ViewHolder();
+
+			holder.txtNome = (TextView) convertView.findViewById(R.id.txtNome);
+			holder.txtDataNascimento = (TextView) convertView
+					.findViewById(R.id.txtDataNascimento);
+
+			convertView.setTag(holder);
+
+		} else {
+			holder = (ViewHolder) convertView.getTag();
+		}
+
+
+		Cliente cliente = clientes.get(posicao);
+		holder.txtNome.setText(cliente.getNome());
+		holder.txtDataNascimento.setText(cliente.getDataNascimento().toGMTString());
+
+		return convertView;
+	}
+
+}
---
+++ src/com/example/exemplolistviewobjeto/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.exemplolistviewobjeto;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class MainActivity extends ListActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        List<Cliente> clientes = new ArrayList<Cliente>();
+        
+        clientes.add(new Cliente("Fulaninha", new Date(1999, 3, 12)));
+        clientes.add(new Cliente("Sicrano", new Date(1980, 4, 2)));
+        clientes.add(new Cliente("Beltrano", new Date(1989, 6, 20)));
+        clientes.add(new Cliente("Fulaninha", new Date(1999, 3, 12)));
+        clientes.add(new Cliente("Sicrano", new Date(1980, 4, 2)));
+        clientes.add(new Cliente("Beltrano", new Date(1989, 6, 20)));
+        clientes.add(new Cliente("Fulaninha", new Date(1999, 3, 12)));
+        clientes.add(new Cliente("Sicrano", new Date(1980, 4, 2)));
+        clientes.add(new Cliente("Beltrano", new Date(1989, 6, 20)));
+        clientes.add(new Cliente("Fulaninha", new Date(1999, 3, 12)));
+        clientes.add(new Cliente("Sicrano", new Date(1980, 4, 2)));
+        clientes.add(new Cliente("Beltrano", new Date(1989, 6, 20)));
+       
+        setListAdapter(new ClienteAdapter(this, clientes));
+        
+    }
+
+}
